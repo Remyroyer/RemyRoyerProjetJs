b4f7c784664aa8b5a706c2430bbf119cedcbd2ba
Remyroyer <98874977+Remyroyer@users.noreply.github.com>
2022-08-31 12:02:52 +0200

    Create exo.js

---
+++ exo.js
@@ -0,0 +1,13 @@
+let numberOfSeasons = 6;
+
+let numberOfEpisodes = 12;
+
+let episodeTime = 45;
+
+let commercialTime = 5;
+
+let tampon = (numberOfEpisodes * numberOfSeasons * episodeTime);
+
+let totalShowTime = tampon - (tampon * commercialTime);
+
+console.log(totalShowTime);
